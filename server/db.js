4fe68e17803fc1984404dea59213fec8aa03dae0
AD <ad@it-eburg.ru>
2021-08-02 23:48:33 +0500

    getData in 70%

--- server/db.js
+++ server/db.js
@@ -31,24 +31,50 @@ defResponse = {
 }
 
 function getLessonsList(searchParams) {
-    // console.log(searchParams);
-    let answer = db.select('*').from('lessons').where((builder) => {
-        // builder.where('id', '>', 0); //Заглушка, чтобы дальше цеплять andWhere
-        // if (searchParams.date) builder.andWhere('date',searchParams.date);
-        // if (searchParams.status) builder.andWhere('status',searchParams.status);
+    if (!searchParams.page) {searchParams.page = 1}
+    if (!searchParams.lessonsPerPage) {searchParams.lessonsPerPage = 5}
+    offset = (searchParams.page - 1) * searchParams.lessonsPerPage;
+    let answer = db.select('lessons.*', db.raw('(select count(visit) as "visitCount" from lesson_students ls where visit = true and lesson_id = lessons.id group by lesson_id)'))
+        .from('lessons').offset(offset).limit(searchParams.lessonsPerPage).where((builder) => {
+        if (searchParams.date) {
+            let { date } = searchParams;
+            let dateArr = date.split(',');
+            if (dateArr.length < 2) {
+                builder.where('date',searchParams.date);
+            } else {
+                let maxDate, minDate;
+                if (Date.parse(dateArr[1]) >= Date.parse(dateArr[0])) {
+                    maxDate=dateArr[1];
+                    minDate=dateArr[0];
+                } else {
+                    maxDate=dateArr[0];
+                    minDate=dateArr[1];
+                }
+                builder.where('date', '>=', minDate);
+                builder.where('date', '<=', maxDate);
+            }
+        }
+        if (searchParams.status) builder.andWhere('status',searchParams.status);
         if (searchParams.teacherIds)  {
             builder.whereIn('id', function() {
                 this.select('lesson_id').from('lesson_teachers').whereIn('teacher_id',searchParams.teacherIds.split(','));
             });
         }
-        
-        // if (searchParams.teacherIds)  {
-        //     builder.andWhere((builder2) => {
-        //         builder2.whereIn('id', function() {
-        //             this.select('lesson_id').from('lesson_teachers').whereIn('teacher_id',searchParams.teacherIds.split(','));
-        //         });
-        //     });
-        // }
+        if (searchParams.studentsCount) {
+            let { studentsCount } = searchParams;
+            if (Number(studentsCount)) {
+                builder.whereIn('id', function() {
+                    this.select('lesson_id').from('lesson_students').groupBy('lesson_id')
+                        .havingRaw('COUNT (student_id) = ' + studentsCount);
+                });
+            } else {
+                const studentsCountArr = studentsCount.split(',');
+                builder.whereIn('id', function() {
+                    this.select('lesson_id').from('lesson_students').groupBy('lesson_id')
+                        .havingRaw('COUNT (student_id) between ' + Math.min.apply(null, studentsCountArr) + ' and ' + Math.max.apply(null, studentsCountArr));
+                });
+            }
+        }
     })
     console.log(answer.toString());
     return answer;
